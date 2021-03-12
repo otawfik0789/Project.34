143035b138b208880ec368009e9521659b241b4c
otawfik0789 <79497773+otawfik0789@users.noreply.github.com>
2021-03-11 23:48:46 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+
+function preload() {
+//preload the images here
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+}
+
+function draw() {
+  background(0);
+
+
+
+  function mouseDragged() {
+    matter.Body.setPosition(hero.body, { x: mouseX, y: mouseY });
+  }
+{
+  this.image=loadImage("Monster-01.png");
+  this.body=Bodies.circle(this.x, this.y, (this.r))/2, options}
+  AudioWorkletNode.add(world, this.body);
+}
+
